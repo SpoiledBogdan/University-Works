57facea7c28b045ccbbc29bd44853a87e87d8ad7
SpoiledBogdan <52323913+SpoiledBogdan@users.noreply.github.com>
2021-10-05 17:30:35 +0300

    Update first_lab.cpp

--- first_lab.cpp
+++ first_lab.cpp
@@ -14,31 +14,31 @@ public:
         m_height = height;
         m_length = length;
         m_width = width;
-        cout << "\nÊîíñòðóêòîð èíèöèàëèçàöèè âûçâàí.\n";
+        cout << "\nКонструктор инициализации вызван.\n";
     }
 
-    Paral() : m_height(1.3f), m_length(2.2f), m_width(7.1f)        // Êîíñòðóêòîð áåç ïàðàìåòðîâ
+    Paral() : m_height(1.3f), m_length(2.2f), m_width(7.1f)        // Конструктор без параметров
     {
-        cout << "\nÊîíñòðóêòîð áåç ïàðàìåòðîâ âûçâàí.\n";
+        cout << "\nКонструктор без параметров вызван.\n";
     }
 
     Paral(const Paral& other) : m_height(other.m_height), m_length(other.m_length), m_width(other.m_width)
     {
-        cout << "\nÊîíñòðóêòîð êîïèðîâàíèÿ âûçâàí.\n";
+        cout << "\nКонструктор копирования вызван.\n";
     }
 
     ~Paral() {
-        cout << "\nÄåñòðóêòîð îáúåêòà âûçâàí.\n";
+        cout << "\nДеструктор объекта вызван.\n";
     }
 
     float find_square() {
         float m_square = 2 * m_length * m_width + 2 * m_width * m_height + 2 * m_length * m_height;
-        cout << "\nÏëîùàäü ïîâåðõíîñòè ðàâíà: " << m_square << " ñì3.\n";
+        cout << "\nПлощадь поверхности равна: " << m_square << " см3.\n";
         return m_square;
     }
     float find_edge_sum() {
         float m_edges_sum = 4 * (m_height + m_length + m_width);
-        cout << "\nÑóììà âñåõ ðåáåð ðàâíà: " << m_edges_sum << " ñì3.\n";
+        cout << "\nСумма всех ребер равна: " << m_edges_sum << " см3.\n";
         return m_edges_sum;
     }
 
@@ -48,33 +48,33 @@ public:
 
 void info_view(Paral& figure)
 {
-    cout <<"\nÎáúåêò êëàññà Paral:"
-        << "\nÂûñîòà ðàâíà: " << figure.m_height << " ñì."
-        << "\nÄëèíà ðàâíà: " << figure.m_length << " ñì."
-        << "\nØèðèòà ðàâíà: " << figure.m_width << " ñì.\n";
+    cout <<"\nОбъект класса Paral:"
+        << "\nВысота равна: " << figure.m_height << " см."
+        << "\nДлина равна: " << figure.m_length << " см."
+        << "\nШирита равна: " << figure.m_width << " см.\n";
 }
 
 
 
 int main() {
-    setlocale(LC_ALL, "Russian"); 
+    setlocale(LC_ALL, "Russian");
 
-    // Ñîçäàíèå òðåõ îáúåêòîâ
+    // Создание трех объектов
     Paral object1;
     Paral object2(5.0f, 5.0f, 5.0f);
     Paral object3(object1);
 
-    // Óêàçàòåëè íà îáúåêòû
+    // Указатели на объекты
     Paral* pointer1 = &object1;
     Paral* pointer2 = &object2;
     Paral* pointer3 = &object3;
 
-    // Ôóíêöèÿ èíôîðìàöèè îáúåêòà
+    // Функция информации объекта
     info_view(object1);
     info_view(object2);
     info_view(object3);
 
-    // Ìåòîäû îáúåêòîâ
+    // Методы объектов
     object1.find_edge_sum();
     object1.find_square();
 
@@ -84,7 +84,7 @@ int main() {
     object3.find_edge_sum();
     object3.find_square();
 
-    // Äîñòóï ê ïîëÿì êëàññà
+    // Доступ к полям класса
     object1.m_height = 3.4f;
     object2.m_length = 3.4f;
     object3.m_width = 3.4f;
